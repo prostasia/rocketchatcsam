43b9e27653ec95e9ad780c4c998e57cce05a097d
Marco Descher <marco@descher.at>
2021-01-13 16:52:04 +0100

    Remove superfluous src directory

--- src/Settings.ts
+++
@@ -1,3 +0,0 @@
-export const SETTING_PHOTODNA_API_KEY: string = "photodna-api-key";
-export const SETTING_QUARANTINE_CHANNEL: string = "csem-quarantine-target-channel";
-export const SETTING_LIMIT_ANALYSIS_TO_CHANNELS = "limit-analysis-to-channels-csv";
--- src/helper/IImageData.ts
+++
@@ -1,4 +0,0 @@
-export interface IImageData {
-    contentType: string;
-    data: Buffer;
-}
